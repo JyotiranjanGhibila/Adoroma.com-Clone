f21971d83dede46cf8786fbc0b00d99af2577067
alih6051 <alih6051@gmail.com>
2023-02-21 20:44:43 +0530

    initial setup

---
+++ Server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(4500, () => {
+  console.log("RUNNING AT PORT 4500");
+});
--- Server/readme.md
+++
@@ -1,3 +0,0 @@
-👉this is for server side you can put your backend related work.
-
-👉daily create branch not at a time 
