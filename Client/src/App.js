294605442d0657f0e0579cb776f0f38926c857ef
unknown <vermabraj>
2023-02-21 20:10:18 +0530

    added basic navbar

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Navbar from "./Components/Navbar"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
     </div>
   );
 }
