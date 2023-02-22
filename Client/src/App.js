07fb4ce7d2625631c7bec44d480cc067cfa442c6
JyotiranjanGhibila <ghibilamohan@gmail.com>
2023-02-22 11:40:48 +0530

    pull

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Camera from './Components/Products/Camera';
 
 function App() {
   return (
     <div className="App">
-     
+     <Camera/>
     </div>
   );
 }
---
+++ Client/src/Components/Products/Camera.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Camera = () => {
+  return (
+    <div>Camera</div>
+  )
+}
+
+export default Camera
