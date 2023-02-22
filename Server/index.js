42a40a26d7786f2ffe46afd56d675d39adf754f4
alih6051 <alih6051@gmail.com>
2023-02-22 15:34:28 +0530

    login & register completed

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const { connection } = require("./configs/db");
+const { UserRouter } = require("./routes/User.route");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(4500, () => {
+// HOME
+app.get("/", (req, res) => {
+  res.send("ELECTRO GLAD");
+});
+
+// ROUTES
+app.use("/api/users", UserRouter);
+
+app.listen(4500, async () => {
+  try {
+    await connection;
+    console.log("CONNECTED TO DB");
+  } catch (error) {
+    console.log(error);
+  }
   console.log("RUNNING AT PORT 4500");
 });
