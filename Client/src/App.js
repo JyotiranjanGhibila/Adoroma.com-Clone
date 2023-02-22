4f21411981fd145a3487dffc9de0458e0e00e3bc
unknown <vermabraj>
2023-02-22 23:09:04 +0530

    fw20_0997_day_2

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,13 @@
-import Navbar from "./Components/Navbar"
+import Navbar from "./Components/Navbar/Navbar"
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
      <Navbar/> 
+    
     </div>
   );
 }
