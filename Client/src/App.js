b7a15dfb79fa20a8b288adf11d26bfe445c54c65
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-02-22 22:04:06 +0530

    fw19_0266_day_2

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,13 @@
-import Navbar from "./Components/Navbar"
-import './App.css';
-import Camera from './Components/Products/Camera';
+import Navbar from "./Components/Navbar";
+import "./App.css";
+import Camera from "./Components/Products/Camera";
+import AddToCartButton from "./Components/cartComponents/AddtoCartModal";
 
 function App() {
   return (
     <div className="App">
-
+      <AddToCartButton />
+      
     </div>
   );
 }
