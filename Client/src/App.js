5149876190025235f30d1c419edf386bfe341982
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-02-24 23:42:00 +0530

    fw19_0266_day4

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,13 +4,16 @@ import Camera from "./Components/Products/Camera";
 import AddToCartButton from "./Components/cartComponents/AddtoCartModal";
 import AllComponents from "./Components/AllComponents";
 import CartScreen from "./Components/CartScreen/CartScreen";
+import CartAddress from "./Components/Checkout/Checkout";
+
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
       {/* <AddToCartButton /> */}
-      <CartScreen />
+      {/* <CartScreen /> */}
       {/* <AllComponents/> */}
+      <CartAddress />
     </div>
   );
 }
