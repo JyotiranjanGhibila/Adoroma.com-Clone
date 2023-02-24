ef7ba6ee70699340d3d4af843e3b35fbe1b805f2
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-02-24 14:32:25 +0530

    shopping cart and cart sumeery

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,15 +2,17 @@ import Navbar from "./Components/Navbar";
 import "./App.css";
 import Camera from "./Components/Products/Camera";
 import AddToCartButton from "./Components/cartComponents/AddtoCartModal";
-import AllComponents from "./Components/AllComponents"
+import AllComponents from "./Components/AllComponents";
+import CartScreen from "./Components/CartScreen/CartScreen";
 function App() {
   return (
     <div className="App">
-      <AddToCartButton />
-       <Navbar/> 
-     <AllComponents/>
-      </div>
+      {/* <Navbar /> */}
+      {/* <AddToCartButton /> */}
+      <CartScreen />
+      {/* <AllComponents/> */}
+    </div>
   );
 }
 
-export default App
+export default App;
