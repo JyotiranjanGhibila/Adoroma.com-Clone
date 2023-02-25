ac25ef1e3a83b833a5fd523a720d5f7f489bea5a
karmit <karmittech2004@gmail.com>
2023-02-25 19:33:06 +0530

    made changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,7 @@
-// import Navbar from "./Components/Navbar"
 import './App.css';
-// import Camera from './Components/Products/Camera';
-import Allroutes from './Components/Routes/Allroutes';
-import {Link} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Link to="/login">Sign in</Link>
-      <Allroutes/>
     </div>
   );
 }
